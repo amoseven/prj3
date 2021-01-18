0c01f6625d63eef3f2039d809c254b76780aa826
Amoseven <omsk.web@gmail.com>
2021-01-18 14:45:10 +0600

    добавлена Навигационная Панель

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="navbar" v-if="visible">
+    <h3>Freelance</h3>
+    <ul class="navbar-menu">
+      <li><a href="/tasks">Все задачи</a></li>
+      <li>
+        <a href="/new">Создать</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>

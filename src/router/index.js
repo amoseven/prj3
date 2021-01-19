ce396c500186e43f06e4d1c27172b9a65614c68d
Amoseven <omsk.web@gmail.com>
2021-01-19 12:50:01 +0600

    избавился от лишнего объекта, сразу export

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import Tasks from '@/views/Tasks'
 import Task from '@/views/Task'
 import New from '@/views/New'
 
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/tasks', component: Tasks, name: 'tasks', alias: '/'},
     {path: '/new', component: New, name: 'new'},
     {path: '/task/:id?', component: Task, name: 'task'}
-  ]
+  ],
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active'
 })
-
-export default router

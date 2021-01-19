43e2a1b23eb0ff2c2f52ae67b2a24cb5d06af105
Amoseven <omsk.web@gmail.com>
2021-01-19 12:51:46 +0600

    расширен объект справочника статусов задач

--- src/components/AppStatus.vue
+++ src/components/AppStatus.vue
@@ -1,3 +1,41 @@
 <template>
-  <span :class="['badge']">Название статуса</span>
+  <span :class="['badge', status.color]">{{ status.text }}</span>
 </template>
+
+<script>
+export default {
+  name: 'AppStatus',
+  props: ['type'],
+  computed: {
+    status () {
+      return this.statuses[this.type]
+    }
+  },
+  data () {
+    return {
+      statuses: {
+        actived: {
+          color: 'primary',
+          text: 'Активен'
+        },
+        done: {
+          color: 'primary',
+          text: 'Завершён'
+        },
+        executed: {
+          color: 'warning',
+          text: 'Выполняется'
+        },
+        canceled: {
+          color: 'danger',
+          text: 'Отменён'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
